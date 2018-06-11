1ade84009b7cca1367d9ace6e14b16489977aec5
Simon Granström <simon.granstrom@gmail.com>
2018-06-11 20:40:56 +0200

    Initial commit

---
+++ Assets/Script/PlayerMotor.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private const float LANE_DISTANCE = 3.0f;
+    private const float TURN_SPEED = 0.05f;
+
+    //Animation
+    private Animator anim;
+
+    //Movement
+    private CharacterController controller;
+    private float jumpForce = 4.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity;
+    private float speed = 7.0f;
+    private int desiredLane = 1; // 0 = left, 1 = middle, 2 = right
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Gather inputs on which lane we should be
+        //Move Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            MoveLane(false);
+
+        //Move Right
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            MoveLane(true);
+
+        //Calculate where we should be
+        //Start off in the middle lane
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+
+        if (desiredLane == 0)  
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * LANE_DISTANCE;
+
+        //Calculate move delta
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+
+        bool isGrounded = IsGrounded();
+        anim.SetBool("Grounded", isGrounded);
+
+        if (isGrounded) //Grounded
+        {
+            verticalVelocity = -0.1f;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //jump
+                anim.SetTrigger("Jump");
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            //Wont fall down far so termal velocity will never be reached
+            verticalVelocity -= (gravity * Time.deltaTime);
+
+            //Swipe down, fall faster
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = -jumpForce;
+            }
+        }
+
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        //Move the pengu
+        controller.Move(moveVector * Time.deltaTime);
+
+        //Rotate the pengu
+        Vector3 dir = controller.velocity;
+        if (dir != Vector3.zero)
+        {
+            dir.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, dir, TURN_SPEED);
+        }
+    }
+
+    private void MoveLane (bool goingRight)
+    {
+        //If going right, add one, else (left) sub one
+        desiredLane += (goingRight) ? 1 : -1;
+
+        //Clamp between lanes
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+
+    private bool IsGrounded()
+    {
+        Ray groundRay = new Ray(
+            new Vector3(    //Origin
+                controller.bounds.center.x,
+                (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f,
+                controller.bounds.center.z),
+            Vector3.down);   //Direction
+
+        Debug.DrawRay(groundRay.origin, groundRay.direction, Color.cyan, 1.0f);
+
+        return Physics.Raycast(groundRay, 0.2f + 0.1f);
+    }
+}
